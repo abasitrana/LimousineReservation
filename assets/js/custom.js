dfc8aaa41ab9d461d6a817c13c20f589968c6e42
Ovais Ali <ovais.ali20@gmail.com>
2023-12-19 23:34:07 +0500

    Home page UI

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -41,6 +41,57 @@ $(document).ready(function () {
         closePopup();
     });
 
+    
+    $('.testimonial-slider').slick({
+        dots: false,
+        arrows: false,
+        autoplay: true,
+        infinite: true,
+        autoplaySpeed: 4000,
+        cssEase: 'linear',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+        {
+          breakpoint: 1600,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 1440,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 3,
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }]
+    });
+
 
 });
 
